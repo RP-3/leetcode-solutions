6052d637a2295352b9e5b0490f8780d1fd590ae4
Rohan Pethiyagoda <sarith21@gmail.com>
2020-10-03 18:23:26 +1000

    Add O(n) solution to 532

--- 532. K-diff Pairs in an Array.js
+++ 532. K-diff Pairs in an Array.js
@@ -5,7 +5,9 @@
  * @param {number} k
  * @return {number}
  */
-var findPairs = function(nums, k) {
+// O(n.log(n)) time.
+// O(1) space
+ var findPairs = function(nums, k) {
     nums.sort((a, b) => a - b);
     let [result, l, r] = [0, 0, 1];
 
@@ -24,3 +26,27 @@ var findPairs = function(nums, k) {
 
     return result
 };
+
+// O(n) time
+// O(n) space
+var findPairs = function(nums, k) {
+    const seen = new Set();
+    const paired = new Set();
+    let result = 0;
+
+    for(let i=0; i<nums.length; i++){
+        const w = [nums[i] - k, nums[i] + k];
+
+        w.forEach((wanted) => {
+            if(!seen.has(wanted)) return;
+            const pair = wanted < nums[i] ? `${wanted}:${nums[i]}` : `${nums[i]}:${wanted}`;
+            if(paired.has(pair)) return;
+            result++;
+            paired.add(pair)
+        });
+
+        seen.add(nums[i]);
+    }
+
+    return result;
+}
