ec6d4f23ebc44b69033444357e6f778dc3be996c
Rohan Pethiyagoda <sarith21@gmail.com>
2020-11-09 19:21:07 +1100

    Add more common utility methods

--- common.go
+++ common.go
@@ -14,8 +14,23 @@ func max(a, b int) int {
 	return b
 }
 
+// abs returns the absolute value of its input
+func abs(v int) int {
+	if v < 0 {
+		return -v
+	}
+	return v
+}
+
 // ListNode is the standard leetcode definition for singly-linked list.
 type ListNode struct {
 	Val  int
 	Next *ListNode
+}
+
+// TreeNode is a standard leetcode definition for a binary tree node
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
 }
